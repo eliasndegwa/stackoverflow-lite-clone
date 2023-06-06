874d1fdb62addaa7468043ae501aafaa62b1efb6
eliasndegwa <eliasndegwamwai@gmail.com>
2023-06-06 10:37:24 +0300

    add starter frontend files created a branch for frontend add standalone header component

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from "./header/header.component";
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderComponent
+    ]
+})
+export class AppModule { }
